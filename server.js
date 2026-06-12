a4cea3b0ddfbb2b1fca958cb4a42dede78d54e46
josesan <sanchezjoseraul770@gmail.com>
2026-06-12 16:21:38 -0600

    proxy dinamico

--- server.js
+++ server.js
@@ -10,15 +10,29 @@ app.get("/", (req, res) => {
     res.send("TVLEGAL PROXY funcionando");
 });
 
-app.use("/stream", createProxyMiddleware({
-    target: "http://190.61.101.11:7050",
-    changeOrigin: true,
-    pathRewrite: {
-        "^/stream": ""
-    }
-}));
+app.use("/proxy", (req, res, next) => {
 
-const PORT = process.env.PORT || 3000;
+    const url = req.query.url;
+
+    if (!url) {
+        return res.status(400).send("Falta parametro url");
+    }
+
+    createProxyMiddleware({
+        target: url,
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: () => "",
+        router: () => url,
+        onProxyReq(proxyReq) {
+            proxyReq.setHeader("Origin", "");
+            proxyReq.setHeader("Referer", "");
+        }
+    })(req, res, next);
+
+});
+
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
     console.log("Servidor activo en puerto " + PORT);
